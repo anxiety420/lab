875c4ca9c1c28edb650f06663b7eb789a98f8dab
anxiety420 <kusakab6ee@gmail.com>
2025-05-20 19:02:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const browserInfo = {
 };
 localStorage.setItem('systemInfo', JSON.stringify(browserInfo));
 
+
 const footer = document.getElementById('footerInfo');
 const info = JSON.parse(localStorage.getItem('systemInfo'));
 footer.innerHTML = '<pre>' + JSON.stringify(info, null, 2) + '</pre>';
+
 
 fetch('https://jsonplaceholder.typicode.com/posts/11/comments')
   .then(response => response.json())
@@ -19,14 +21,30 @@ fetch('https://jsonplaceholder.typicode.com/posts/11/comments')
       div.innerHTML = `<h3>${comment.name}</h3><p>${comment.body}</p><hr>`;
       container.appendChild(div);
     });
-  });
+  
+    for(let i = 1; i <= 5; i++) {
+      const div = document.createElement('div');
+      div.innerHTML = `<p>for: номер ${i}</p>`;
+      container.appendChild(div);
+    }
+  
+    let j = 0;
+    while (j < 5) {
+      const div = document.createElement('div');
+      div.innerHTML = `<p>while: номер ${j}</p>`;
+      container.appendChild(div);
+      j++;
+    }
+});
 
 setTimeout(() => {
   document.getElementById('feedbackModal').style.display = 'flex';
 }, 60000);
 
+
 const hour = new Date().getHours();
 document.body.classList.add(hour >= 7 && hour < 21 ? 'day' : 'night');
+
 
 function toggleTheme() {
   if (document.body.classList.contains('day')) {
